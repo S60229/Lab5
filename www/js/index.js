74c3fecdd3deb8ae958cb2909c4e5f1059354c05
S60229 <103551596+S60229@users.noreply.github.com>
2022-06-25 14:26:48 +0800

    Add files via upload

---
+++ www/js/index.js
@@ -0,0 +1,13 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+if (!sessionStorage.ttoken || sessionStorage.ttoken == null)
+    location.href = "login.html";
+
+
+
+
+
+
